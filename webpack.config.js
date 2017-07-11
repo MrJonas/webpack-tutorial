b09818d1c0ddb29359a135e4ac68cf522b17ef90
Jonas <jonas@Jonass-MacBook-Air.local>
2017-07-11 21:37:13 +0300

    finished vendor spliting example

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
-
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const VENDOR_LIBS = [
 
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
       rules: [
@@ -33,8 +33,11 @@ module.exports = {
   },
    plugins: [
        new webpack.optimize.CommonsChunkPlugin({
-          name: 'vendor'
-       })
+          names: ['vendor', 'manifest']
+       }),
+       new HtmlWebpackPlugin({
+           template: 'src/index.html'
+       }),
    ]
 
 
